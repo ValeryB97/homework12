e517ea111406c919e7214df2a3aa1ed671fbdafe
ValeryB97 <valery.babaryko@gmail.com>
2024-05-08 19:36:04 +0700

    Homework 13 done.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String nameBook;
     private Author author;
@@ -23,5 +25,23 @@ public class Book {
 
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(author);
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + nameBook + ". Автор - " + author + ". Год выпуска - " + publishingYear;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,9 @@ public class Main {
         Book captainDaughter = new Book("Капитанская дочка", pushkin, 1836);
         Author chekhov = new Author("Антон", "Чехов");
         Book chameleon = new Book("Хамелеон", chekhov, 1884);
-        System.out.println(captainDaughter.getNameBook());
-        System.out.println(captainDaughter.getAuthor());
-        System.out.println(captainDaughter.getPublishingYear());
-        System.out.println(chameleon.getNameBook());
-        System.out.println(chameleon.getAuthor());
-        System.out.println(chameleon.getPublishingYear());
+        System.out.println(captainDaughter.toString());
+        System.out.println(chameleon.toString());
         chameleon.setPublishingYear(1985);
-        System.out.println(chameleon.getPublishingYear());
+        System.out.println(chameleon.toString());
     }
 }
